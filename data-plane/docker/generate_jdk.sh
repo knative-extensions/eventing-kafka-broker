c2aa1da12c3f2be26f8ecf3ab9555c5af89b7664
slinkydeveloper <francescoguard@gmail.com>
2020-10-07 18:26:07 +0200

    Now it should contain all the mods we need
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- data-plane/docker/generate_jdk.sh
+++ data-plane/docker/generate_jdk.sh
@@ -6,7 +6,13 @@ if [ $# -eq 0 ]
 fi
 
 echo "Computing mods for $1"
-MODS=$(jdeps -q --print-module-deps --ignore-missing-deps "$1" | sed -e 's/^[ \t]*//' | sed -z 's/\n/,/g;s/,$/\n/')
-echo "Modules = '$MODS'"
+MODS=$(jdeps -q --print-module-deps --ignore-missing-deps "$1" | sed -e 's/^[ \t]*//')
+
+echo "Computed mods = '$MODS'"
+
+# Patch adding the dns
+MODS="$MODS,jdk.naming.dns"
+
+echo "Patched modules shipped with the generated jdk = '$MODS'"
 jlink --verbose --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules "$MODS" --output /app/jdk
 
