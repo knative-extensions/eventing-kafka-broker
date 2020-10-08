bcc75db53b2feb770078eae626d5c319ab6251eb
slinkydeveloper <francescoguard@gmail.com>
2020-10-08 12:49:07 +0200

    java.sql is needed
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- data-plane/docker/generate_jdk.sh
+++ data-plane/docker/generate_jdk.sh
@@ -10,7 +10,7 @@ MODS=$(jdeps -q --print-module-deps --ignore-missing-deps "$1")
 echo "Computed mods = '$MODS'"
 
 # Remove compiler, sql, management modules
-MODS=$(echo $MODS | tr , '\n' | sed '/^java.compiler/d' | sed '/^java.sql/d' | sed '/^jdk.management/d' | sed -z 's/\n/,/g;s/,$/\n/')
+MODS=$(echo $MODS | tr , '\n' | sed '/^java.compiler/d' | sed '/^jdk.management/d' | sed -z 's/\n/,/g;s/,$/\n/')
 # Patch adding the dns
 MODS="$MODS,jdk.naming.dns"
 
