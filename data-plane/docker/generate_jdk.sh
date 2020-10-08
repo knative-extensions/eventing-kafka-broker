212fe41dc58aa88029c3be430a00c27f4357aacb
slinkydeveloper <francescoguard@gmail.com>
2020-10-08 12:39:11 +0200

    Play with modules
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- data-plane/docker/generate_jdk.sh
+++ data-plane/docker/generate_jdk.sh
@@ -6,10 +6,11 @@ if [ $# -eq 0 ]
 fi
 
 echo "Computing mods for $1"
-MODS=$(jdeps -q --print-module-deps --ignore-missing-deps "$1" | sed -e 's/^[ \t]*//')
-
+MODS=$(jdeps -q --print-module-deps --ignore-missing-deps "$1")
 echo "Computed mods = '$MODS'"
 
+# Remove compiler, sql, management modules
+MODS=$(echo $MODS | tr , '\n' | sed '/^java.compiler/d' | sed '/^java.sql/d' | sed '/^jdk.management/d' | sed -z 's/\n/,/g;s/,$/\n/')
 # Patch adding the dns
 MODS="$MODS,jdk.naming.dns"
 
