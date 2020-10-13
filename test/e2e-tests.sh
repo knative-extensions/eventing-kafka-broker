8848577a881b2b1042a71b92f25bdb1404e4faf3
Pierangelo Di Pilato <pierangelodipilato@gmail.com>
2020-10-13 09:30:40 +0200

    kip heavy workloads installation using LOCAL_DEVELOPMENT env var
    Signed-off-by: Pierangelo Di Pilato <pierangelodipilato@gmail.com>

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # variables used:
-# - SKIP_INITIALIZE (default: false) - skip cluster creation
+# - SKIP_INITIALIZE (default: false) - skip cluster creation.
+# - LOCAL_DEVELOPMENT (default: false) - skip heavy workloads installation like load and chaos generators.
+
+readonly SKIP_INITIALIZE=${SKIP_INITIALIZE:-false}
+readonly LOCAL_DEVELOPMENT=${LOCAL_DEVELOPMENT:-false}
 
 source $(dirname $0)/e2e-common.sh
 
@@ -18,25 +22,35 @@ if ! ${SKIP_INITIALIZE}; then
   initialize $@ --skip-istio-addon
 fi
 
-apply_chaos || fail_test "Failed to apply chaos"
+if ! ${LOCAL_DEVELOPMENT}; then
+  apply_chaos || fail_test "Failed to apply chaos"
+fi
+
+if ! ${LOCAL_DEVELOPMENT}; then
+  apply_sacura || fail_test "Failed to apply Sacura"
+fi
 
 header "Waiting Knative eventing to come up"
 
-apply_sacura || fail_test "Failed to apply Sacura"
+wait_until_pods_running knative-eventing || fail_test "Pods in knative-eventing didn't come up"
 
 header "Running tests"
 
 go_test_e2e -timeout=30m ./test/... || fail_test "Integration test failed"
 
-kubectl wait --for=condition=complete --timeout=10m -n sacura job/sacura || job_completed=$?
+if ! ${LOCAL_DEVELOPMENT}; then
 
-# Export logs from pods in the namespace sacura
-go run test/cmd/logs-exporter/main.go --logs-namespace sacura
+  kubectl wait --for=condition=complete --timeout=10m -n sacura job/sacura || job_completed=$?
 
-if [[ "${job_completed}" -ne "0" ]]; then
-  kubectl logs -n knative-eventing -l=app=sacura
-  kubectl describe -n knative-eventing job sacura
-  fail_test "Sacura Job not completed"
+  # Export logs from pods in the namespace sacura.
+  go run test/cmd/logs-exporter/main.go --logs-namespace sacura
+
+  if [[ "${job_completed}" -ne "0" ]]; then
+    kubectl logs -n sacura -l=app=sacura
+    kubectl describe -n sacura job sacura
+    fail_test "Sacura Job not completed"
+  fi
+
 fi
 
 success
