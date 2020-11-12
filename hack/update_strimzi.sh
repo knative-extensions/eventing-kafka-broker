8f3d0d7867bdd2a247e98ab2db71025722e461dc
Pierangelo Di Pilato <pierangelodipilato@gmail.com>
2020-11-12 10:52:13 +0100

    Format
    Signed-off-by: Pierangelo Di Pilato <pierangelodipilato@gmail.com>

--- hack/update_strimzi.sh
+++ hack/update_strimzi.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-# After running this script you need to add the env variable KUBERNETES_SERVICE_DNS_DOMAIN to .cluster.local in order to use 
-# a different DNS suffix to the strimzi-cluster-operator Deployment.
+# After running this script you need to add the env variable KUBERNETES_SERVICE_DNS_DOMAIN
+# to .cluster.local to the strimzi-cluster-operator Deployment in order to use a different
+# DNS suffix.
 # `.cluster.local` will be replaced to the generated one on GitHub Actions.
 
 strimzi_version=$(curl https://github.com/strimzi/strimzi-kafka-operator/releases/latest | awk -F 'tag/' '{print $2}' | awk -F '"' '{print $1}' 2>/dev/null)
