2ed67c7229bba6656639e0cda947ca6a18547206
Pierangelo Di Pilato <pierangelodipilato@gmail.com>
2020-11-12 10:42:16 +0100

    Change KUBERNETES_SERVICE_DNS_DOMAIN based on CLUSTER_SUFFIX
    - https://github.com/strimzi/strimzi-kafka-operator/issues/3945
    
    Signed-off-by: Pierangelo Di Pilato <pierangelodipilato@gmail.com>

---
+++ hack/update_strimzi.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# After running this script you need to add the env variable KUBERNETES_SERVICE_DNS_DOMAIN to .cluster.local in order to use 
+# a different DNS suffix to the strimzi-cluster-operator Deployment.
+# `.cluster.local` will be replaced to the generated one on GitHub Actions.
+
+strimzi_version=$(curl https://github.com/strimzi/strimzi-kafka-operator/releases/latest | awk -F 'tag/' '{print $2}' | awk -F '"' '{print $1}' 2>/dev/null)
+
+echo "Using Strimzi Version: ${strimzi_version}"
+
+curl -L "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml" |
+  sed 's/namespace: .*/namespace: kafka/' > test/kafka/strimzi-cluster-operator.yaml
