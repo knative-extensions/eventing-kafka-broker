ce68cceb02ba2c8ac2466eb87c11fdd62e299d52
Pierangelo Di Pilato <pierangelodipilato@gmail.com>
2020-11-12 16:59:53 +0100

    Add docs to update-strimzi.sh script and rename it
    Signed-off-by: Pierangelo Di Pilato <pierangelodipilato@gmail.com>

--- hack/update-strimzi.sh
+++ hack/update-strimzi.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-# After running this script you need to add the env variable KUBERNETES_SERVICE_DNS_DOMAIN
-# to .cluster.local to the strimzi-cluster-operator Deployment in order to use a different
+# This script updates test/kafka/strimzi-cluster-operator.yaml
+#
+# After running this script you need to set the env variable KUBERNETES_SERVICE_DNS_DOMAIN
+# to cluster.local to the strimzi-cluster-operator Deployment in order to use a different
 # DNS suffix.
-# `.cluster.local` will be replaced to the generated one on GitHub Actions.
+# `cluster.local` will be replaced to the generated one on GitHub Actions.
 
 strimzi_version=$(curl https://github.com/strimzi/strimzi-kafka-operator/releases/latest | awk -F 'tag/' '{print $2}' | awk -F '"' '{print $1}' 2>/dev/null)
 
