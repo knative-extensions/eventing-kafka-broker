7d74e67c98cad8febe521dafb4148c06e7e50f8b
Pierangelo Di Pilato <pierangelodipilato@gmail.com>
2021-01-27 10:56:28 +0100

    Update codegen
    Signed-off-by: Pierangelo Di Pilato <pierangelodipilato@gmail.com>

--- control-plane/pkg/client/listers/eventing/v1alpha1/kafkasink.go
+++ control-plane/pkg/client/listers/eventing/v1alpha1/kafkasink.go
@@ -26,8 +26,10 @@ import (
 )
 
 // KafkaSinkLister helps list KafkaSinks.
+// All objects returned here must be treated as read-only.
 type KafkaSinkLister interface {
 	// List lists all KafkaSinks in the indexer.
+	// Objects returned here must be treated as read-only.
 	List(selector labels.Selector) (ret []*v1alpha1.KafkaSink, err error)
 	// KafkaSinks returns an object that can list and get KafkaSinks.
 	KafkaSinks(namespace string) KafkaSinkNamespaceLister
@@ -58,10 +60,13 @@ func (s *kafkaSinkLister) KafkaSinks(namespace string) KafkaSinkNamespaceLister 
 }
 
 // KafkaSinkNamespaceLister helps list and get KafkaSinks.
+// All objects returned here must be treated as read-only.
 type KafkaSinkNamespaceLister interface {
 	// List lists all KafkaSinks in the indexer for a given namespace.
+	// Objects returned here must be treated as read-only.
 	List(selector labels.Selector) (ret []*v1alpha1.KafkaSink, err error)
 	// Get retrieves the KafkaSink from the indexer for a given namespace and name.
+	// Objects returned here must be treated as read-only.
 	Get(name string) (*v1alpha1.KafkaSink, error)
 	KafkaSinkNamespaceListerExpansion
 }
