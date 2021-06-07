6f5b846d953cc977d0e89e8e977e7cf238d081fc
slinkydeveloper <francescoguard@gmail.com>
2021-06-07 16:24:11 +0200

    Added tests
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

---
+++ data-plane/core/src/test/java/dev/knative/eventing/kafka/broker/core/filter/impl/PrefixFilterTest.java
@@ -0,0 +1,48 @@
+package dev.knative.eventing.kafka.broker.core.filter.impl;
+
+import io.cloudevents.CloudEvent;
+import io.cloudevents.core.builder.CloudEventBuilder;
+import java.net.URI;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrefixFilterTest {
+
+  final static CloudEvent event = CloudEventBuilder.v1()
+    .withId("123-42")
+    .withDataContentType("application/cloudevents+json")
+    .withDataSchema(URI.create("/api/schema"))
+    .withSource(URI.create("/api/some-source"))
+    .withSubject("a-subject-42")
+    .withType("type")
+    .withTime(OffsetDateTime.of(
+      1985, 4, 12,
+      23, 20, 50, 0,
+      ZoneOffset.UTC
+    ))
+    .build();
+
+  @ParameterizedTest
+  @MethodSource(value = {"testCases"})
+  public void match(CloudEvent event, String key, String value, boolean shouldMatch) {
+    var filter = new PrefixFilter(key, value);
+    assertThat(filter.test(event))
+      .isEqualTo(shouldMatch);
+  }
+
+  static Stream<Arguments> testCases() {
+    return Stream.of(
+      Arguments.of(event, "id", "123", true),
+      Arguments.of(event, "id", "124", false),
+      Arguments.of(event, "source", "/api", true),
+      Arguments.of(event, "source", "/news", false)
+    );
+  }
+
+}
