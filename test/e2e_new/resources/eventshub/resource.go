e483d8e7795e73058008308bfa239845e12a8069
slinkydeveloper <francescoguard@gmail.com>
2021-07-07 12:19:38 +0200

    Added options and matchers
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

---
+++ test/e2e_new/resources/eventshub/resource.go
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2021 The Knative Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eventshub
+
+import (
+	"context"
+	"knative.dev/reconciler-test/pkg/environment"
+	"knative.dev/reconciler-test/pkg/eventshub"
+	"strings"
+)
+
+func init() {
+	environment.RegisterPackage(testImagePackage)
+}
+
+const (
+	testImagePackage    = "knative.dev/eventing-kafka-broker/test/test_images/eventshub"
+	kafkaEventsHubImage = "ko://" + testImagePackage
+)
+
+// WithKafkaEventsHub uses the kafka eventshub image when invoking eventshub.Install
+var WithKafkaEventsHub = eventshub.WithCustomImage(kafkaEventsHubImage)
+
+func StartKafkaConsumer(bootstrapServers []string, topic string) eventshub.EventsHubOption {
+	return func(ctx context.Context, m map[string]string) error {
+		if generators, ok := m["EVENT_GENERATORS"]; ok {
+			m["EVENT_GENERATORS"] = generators + "," + "kafkaconsumer"
+		} else {
+			m["EVENT_GENERATORS"] = "kafkaconsumer"
+		}
+
+		m["BOOTSTRAP_SERVERS"] = strings.Join(bootstrapServers, ",")
+		m["TOPIC"] = topic
+		return nil
+	}
+}
