de36074d04c2682eefb8cc040dfd999ee1c49b43
Pierangelo Di Pilato <pdipilat@redhat.com>
2021-10-12 14:01:32 +0200

    Add ConsumerRecord to CloudEvent transformer for KafkaSource
    - Add a new `InvalidCloudEventInterceptor` that is in charge of
      transforming `ConsumerRecords` that aren't CloudEvents to
      valid CloudEvents.
    - Add a new `CloudEventDeserializer` that doesn't fail when
      it tries to deserialize invalid CloudEvents.
    
    Signed-off-by: Pierangelo Di Pilato <pdipilat@redhat.com>

---
+++ data-plane/dispatcher/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/impl/consumer/InvalidCloudEvent.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright © 2018 Knative Authors (knative-dev@googlegroups.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.knative.eventing.kafka.broker.dispatcher.impl.consumer;
+
+import io.cloudevents.CloudEvent;
+import io.cloudevents.CloudEventData;
+import io.cloudevents.SpecVersion;
+
+import java.net.URI;
+import java.time.OffsetDateTime;
+import java.util.Set;
+
+class InvalidCloudEvent implements CloudEvent {
+
+  private final byte[] data;
+
+  InvalidCloudEvent(byte[] data) {
+    this.data = data;
+  }
+
+  public byte[] data() {
+    return this.data;
+  }
+
+  @Override
+  public CloudEventData getData() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public SpecVersion getSpecVersion() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public String getId() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public String getType() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public URI getSource() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public String getDataContentType() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public URI getDataSchema() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public String getSubject() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public OffsetDateTime getTime() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Object getAttribute(String attributeName) throws IllegalArgumentException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Object getExtension(String extensionName) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Set<String> getExtensionNames() {
+    throw new UnsupportedOperationException();
+  }
+}
