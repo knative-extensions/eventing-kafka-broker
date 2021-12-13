96fc2942af00996f648d9ab452760662c24cad3a
aavarghese <avarghese@us.ibm.com>
2021-12-13 12:14:26 -0500

    Fix for unit test
    Signed-off-by: aavarghese <avarghese@us.ibm.com>

--- control-plane/pkg/reconciler/trigger/v2/controllerv2_test.go
+++ control-plane/pkg/reconciler/trigger/v2/controllerv2_test.go
@@ -24,7 +24,6 @@ import (
 	_ "knative.dev/eventing-kafka-broker/control-plane/pkg/client/internals/kafka/injection/informers/eventing/v1alpha1/consumergroup/fake"
 	_ "knative.dev/eventing/pkg/client/injection/informers/eventing/v1/broker/fake"
 	_ "knative.dev/eventing/pkg/client/injection/informers/eventing/v1/trigger/fake"
-	_ "knative.dev/pkg/client/injection/ducks/duck/v1/addressable/fake"
 	_ "knative.dev/pkg/client/injection/kube/informers/core/v1/configmap/fake"
 	_ "knative.dev/pkg/client/injection/kube/informers/core/v1/pod/fake"
 	_ "knative.dev/pkg/client/injection/kube/informers/core/v1/secret/fake"
