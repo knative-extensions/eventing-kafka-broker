994cb1184bf59e6ddc07e55fb1158b4b13037c83
Ali Ok <aliok@redhat.com>
2022-01-11 11:29:04 +0300

    Fix more license headers - year must be 2018 because we use a crappy Maven plugin

--- data-plane/dispatcher/src/test/java/dev/knative/eventing/kafka/broker/dispatcher/impl/ResponseToHttpEndpointHandlerTest.java
+++ data-plane/dispatcher/src/test/java/dev/knative/eventing/kafka/broker/dispatcher/impl/ResponseToHttpEndpointHandlerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2022 Knative Authors (knative-dev@googlegroups.com)
+ * Copyright © 2018 Knative Authors (knative-dev@googlegroups.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
