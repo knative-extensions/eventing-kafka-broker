a9400ebae01e2f26a24770bef55951a1bfb9b31e
Pierangelo Di Pilato <pdipilat@redhat.com>
2022-01-11 16:46:13 +0100

    Add Monitors for all components (#1704)
    * Add Monitors for all components
    
    See https://github.com/knative-sandbox/monitoring for context and usage.
    
    I'm adding `ServiceMonitor`s or `PodMonitor`s for all components so that
    Prometheus can scrape metrics from pods.
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * 2021 -> 2022
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Use redirect instead of append to create manifests
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

---
+++ hack/artifacts-env.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+export EVENTING_KAFKA_CONTROL_PLANE_ARTIFACT="eventing-kafka-controller.yaml"
+export EVENTING_KAFKA_CONTROL_PLANE_PROMETHEUS_OPERATOR_ARTIFACT="eventing-kafka-controller-prometheus-operator.yaml"
+
+export EVENTING_KAFKA_SOURCE_ARTIFACT="eventing-kafka-source.yaml"
+export EVENTING_KAFKA_SOURCE_PROMETHEUS_OPERATOR_ARTIFACT="eventing-kafka-source-prometheus-operator.yaml"
+
+export EVENTING_KAFKA_BROKER_ARTIFACT="eventing-kafka-broker.yaml"
+export EVENTING_KAFKA_BROKER_PROMETHEUS_OPERATOR_ARTIFACT="eventing-kafka-broker-prometheus-operator.yaml"
+
+export EVENTING_KAFKA_SINK_ARTIFACT="eventing-kafka-sink.yaml"
+export EVENTING_KAFKA_SINK_PROMETHEUS_OPERATOR_ARTIFACT="eventing-kafka-sink-prometheus-operator.yaml"
+
+export EVENTING_KAFKA_CHANNEL_ARTIFACT="eventing-kafka-channel.yaml"
+export EVENTING_KAFKA_CHANNEL_PROMETHEUS_OPERATOR_ARTIFACT="eventing-kafka-channel-prometheus-operator.yaml"
+
+export EVENTING_KAFKA_ARTIFACT="eventing-kafka.yaml"
