9e02488963ff1c6b6f01f5dda8fa9895a2c82ab8
Matthias Wessendorf <mwessend@redhat.com>
2022-01-25 13:38:13 +0100

    Apply better release generator
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- openshift/release/generate-release.sh
+++ openshift/release/generate-release.sh
@@ -23,6 +23,10 @@ resolve_resources control-plane/config/100-sink cp_sink.yaml $image_prefix $tag
 cat cp_sink.yaml >> $broker_cp_output_file
 rm cp_sink.yaml
 
+resolve_resources control-plane/config/100-source cp_source.yaml $image_prefix $tag
+cat cp_source.yaml >> $broker_cp_output_file
+rm cp_source.yaml
+
 resolve_resources control-plane/config/200-controller cp_broker.yaml $image_prefix $tag
 cat cp_broker.yaml >> $broker_cp_output_file
 rm cp_broker.yaml
@@ -32,22 +36,20 @@ rm cp_broker.yaml
 
 # the Broker Data Plane folders
 # The generic DP root folder
-resolve_resources data-plane/config $broker_dp_output_file $image_prefix $tag
+#resolve_resources data-plane/config $broker_dp_output_file $image_prefix $tag
 
 # The DP folder for Broker:
-resolve_resources data-plane/config/broker dp_broker.yaml $image_prefix $tag
-cat dp_broker.yaml >> $broker_dp_output_file
-rm dp_broker.yaml
-
-resolve_resources data-plane/config/broker/template dp_broker.yaml $image_prefix $tag
-cat dp_broker.yaml >> $broker_dp_output_file
-rm dp_broker.yaml
+resolve_resources data-plane/config/broker $broker_dp_output_file $image_prefix
+# cat dp_broker.yaml >> $broker_dp_output_file
+# rm dp_broker.yaml
 
 # The DP folder for Sink:
 resolve_resources data-plane/config/sink dp_sink.yaml $image_prefix $tag
 cat dp_sink.yaml >> $broker_dp_output_file
 rm dp_sink.yaml
 
-resolve_resources data-plane/config/sink/template dp_sink.yaml $image_prefix $tag
-cat dp_sink.yaml >> $broker_dp_output_file
-rm dp_sink.yaml
+# The DP folder for Source:
+resolve_resources data-plane/config/source dp_source.yaml $image_prefix $tag
+cat dp_source.yaml >> $broker_dp_output_file
+rm dp_source.yaml
+
