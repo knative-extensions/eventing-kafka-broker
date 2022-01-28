26afd51996f2ab344b4d3f0aa0242ddae00101b3
Ali Ok <aliok@redhat.com>
2022-01-28 14:54:38 +0300

    Run KafkaChannel e2e and Rekt tests in regular case

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -40,6 +40,8 @@ export_logs_continuously
 
 go_test_e2e -timeout=1h ./test/e2e/... || fail_test "E2E suite failed"
 
+go_test_e2e -timeout=1h ./test/e2e_channel/... -channels=messaging.knative.dev/v1beta1:KafkaChannel || fail_test "E2E suite (KafkaChannel) failed"
+
 go_test_e2e -tags=deletecm ./test/e2e/... || fail_test "E2E (deletecm) suite failed"
 
 if ! ${LOCAL_DEVELOPMENT}; then
