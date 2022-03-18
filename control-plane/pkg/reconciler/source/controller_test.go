2bad562a6792889103e6511d8042cca09263546c
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-03-18 11:57:26 +0100

    Fix tests
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- control-plane/pkg/reconciler/source/controller_test.go
+++ control-plane/pkg/reconciler/source/controller_test.go
@@ -19,50 +19,23 @@ package source
 import (
 	"testing"
 
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	fakekubeclientset "k8s.io/client-go/kubernetes/fake"
 	_ "knative.dev/eventing/pkg/client/injection/informers/eventing/v1/broker/fake"
 	_ "knative.dev/eventing/pkg/client/injection/informers/eventing/v1/trigger/fake"
 	_ "knative.dev/pkg/client/injection/ducks/duck/v1/addressable/fake"
-	fakekubeclient "knative.dev/pkg/client/injection/kube/client/fake"
 	_ "knative.dev/pkg/client/injection/kube/informers/core/v1/configmap/fake"
 	_ "knative.dev/pkg/client/injection/kube/informers/core/v1/pod/fake"
 	_ "knative.dev/pkg/client/injection/kube/informers/core/v1/secret/fake"
-	dynamicclient "knative.dev/pkg/injection/clients/dynamicclient/fake"
 	reconcilertesting "knative.dev/pkg/reconciler/testing"
 
 	_ "knative.dev/eventing-kafka/pkg/client/injection/informers/sources/v1beta1/kafkasource/fake"
 
 	_ "knative.dev/eventing-kafka-broker/control-plane/pkg/client/internals/kafka/injection/informers/eventing/v1alpha1/consumergroup/fake"
-
-	"knative.dev/eventing-kafka-broker/control-plane/pkg/config"
 )
 
 func TestNewControllerV2(t *testing.T) {
 	ctx, _ := reconcilertesting.SetupFakeContext(t)
 
-	configs := &config.Env{
-		SystemNamespace:      "cm",
-		GeneralConfigMapName: "cm",
-	}
-
-	ctx, _ = fakekubeclient.With(
-		ctx,
-		&corev1.ConfigMap{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      configs.GeneralConfigMapName,
-				Namespace: configs.SystemNamespace,
-			},
-		},
-	)
-	dynamicScheme := runtime.NewScheme()
-	_ = fakekubeclientset.AddToScheme(dynamicScheme)
-
-	dynamicclient.With(ctx, dynamicScheme)
-
-	controller := NewController(ctx, configs)
+	controller := NewController(ctx)
 	if controller == nil {
 		t.Error("failed to create controller: <nil>")
 	}
