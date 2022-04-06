0f28c5b88bba6dd6686fd615d01b54d007d90f72
Martin Gencur <mgencur@redhat.com>
2022-04-06 08:45:44 +0200

    Enable Zipkin tracing for upgrade tests (#2053)
    * Increase resources for Zipkin
    
    * TMP: Check the current tracing config
    
    * Apply test config-tracing after installation
    
    * Use kubectl
    
    * Use the correct tracing config

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -131,6 +131,9 @@ function install_latest_release() {
   kubectl apply -f "${PREVIOUS_RELEASE_URL}/${EVENTING_KAFKA_SINK_ARTIFACT}" || return $?
   kubectl apply -f "${PREVIOUS_RELEASE_URL}/${EVENTING_KAFKA_SOURCE_ARTIFACT}" || return $?
   # kubectl apply -f "${PREVIOUS_RELEASE_URL}/${EVENTING_KAFKA_CHANNEL_ARTIFACT}" || return $?
+
+  # Restore test config-tracing.
+  kubectl replace -f ./test/config/100-config-tracing.yaml
 }
 
 function install_head() {
@@ -142,6 +145,9 @@ function install_head() {
   kubectl apply -f "${EVENTING_KAFKA_SINK_ARTIFACT}" || return $?
   kubectl apply -f "${EVENTING_KAFKA_CHANNEL_ARTIFACT}" || return $?
   kubectl apply -f "${EVENTING_KAFKA_POST_INSTALL_ARTIFACT}" || return $?
+
+  # Restore test config-tracing.
+  kubectl replace -f ./test/config/100-config-tracing.yaml
 }
 
 function test_setup() {
