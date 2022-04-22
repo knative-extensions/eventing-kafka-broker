2b18b940470b258f84a17089ddf31f43b20008af
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-04-22 12:15:32 +0200

    Run eventing-core tests first
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -27,12 +27,12 @@ if [ "${EVENTING_KAFKA_BROKER_CHANNEL_AUTH_SCENARIO:-""}" != "" ]; then
   success
 fi
 
+# TODO: move this into a separate test job
+run_eventing_core_tests || fail_test "Failed to run eventing core tests"
+
 go_test_e2e -timeout=1h ./test/e2e/... || fail_test "E2E suite failed"
 
 go_test_e2e -timeout=1h ./test/e2e_channel/... -channels=messaging.knative.dev/v1beta1:KafkaChannel || fail_test "E2E suite (KafkaChannel) failed"
-
-# TODO: move this into a separate test job
-run_eventing_core_tests || fail_test "Failed to run eventing core tests"
 
 go_test_e2e -tags=deletecm ./test/e2e/... || fail_test "E2E (deletecm) suite failed"
 
