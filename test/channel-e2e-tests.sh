b453727b48430f74108c326560844104bcd9bd8e
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-06-13 18:00:49 +0200

    Fix reproducer script path
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- test/channel-e2e-tests.sh
+++ test/channel-e2e-tests.sh
@@ -2,6 +2,6 @@
 
 source $(dirname $0)/e2e-common.sh
 
-$(dirname $0)/reproducer.sh || fail_test "Reproducer failed"
+$(dirname $0)/../reproducer.sh || fail_test "Reproducer failed"
 
 go_test_e2e -timeout=1h ./test/e2e_channel/... -channels=messaging.knative.dev/v1beta1:KafkaChannel || fail_test "E2E suite (KafkaChannel) failed"
