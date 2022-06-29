87b01376a493250258706fbaf851c1fe9cacb113
Dirk Haubenreisser <haubenr@de.ibm.com>
2022-06-29 11:51:28 +0200

    Enabled downscaling of chaosduck for e2e tests, disabled chaosduck for channel e2e tests
    Signed-off-by: Dirk Haubenreisser <haubenr@de.ibm.com>

--- test/channel-e2e-tests.sh
+++ test/channel-e2e-tests.sh
@@ -4,6 +4,10 @@ export SCALE_CHAOSDUCK_TO_ZERO=1
 
 source $(dirname $0)/e2e-common.sh
 
+if ! ${LOCAL_DEVELOPMENT}; then
+  scale_chaosduck
+fi
+
 $(dirname $0)/scripts/first-event-delay.sh || fail_test "Failed"
 
 go_test_e2e -timeout=1h ./test/e2e_channel/... -channels=messaging.knative.dev/v1beta1:KafkaChannel || fail_test "E2E suite (KafkaChannel) failed"
