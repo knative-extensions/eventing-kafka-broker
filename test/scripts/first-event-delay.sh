8937dc54a5c5b821b3774ebf4321c78e1164a1a5
Dirk Haubenreisser <haubenr@de.ibm.com>
2022-06-30 16:31:41 +0200

    Force-delete test namespaces

--- test/scripts/first-event-delay.sh
+++ test/scripts/first-event-delay.sh
@@ -174,8 +174,7 @@ function run {
   wait_for_cloudevent foo$i
 
   echo "Force-deleting namespace for test run $i ..."
-  kubectl patch ns/foo$i --type json --patch='[ { "op": "remove", "path": "/metadata/finalizers" } ]'
-  kubectl delete namespace foo$i --grace-period=1 --wait=false
+  kubectl delete namespace foo$i --grace-period=0 --force --wait=false
 
   echo "Ending test run $i ."
 }
