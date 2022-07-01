d4b2a2b1457e28901e7ffd498726ce98d2540588
Dirk Haubenreisser <haubenr@de.ibm.com>
2022-07-01 11:48:32 +0200

    Minor cleanup
    Signed-off-by: Dirk Haubenreisser <haubenr@de.ibm.com>

--- test/channel-e2e-tests.sh
+++ test/channel-e2e-tests.sh
@@ -7,5 +7,3 @@ $(dirname $0)/scripts/first-event-delay.sh || fail_test "Failed"
 
 header "Running e2e_channel go tests ..."
 go_test_e2e -timeout=1h ./test/e2e_channel/... -channels=messaging.knative.dev/v1beta1:KafkaChannel || fail_test "E2E suite (KafkaChannel) failed"
-
-success
