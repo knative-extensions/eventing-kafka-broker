18b965508df0dd2937d9f714d516214ad556f585
Dirk Haubenreisser <haubenr@de.ibm.com>
2022-07-05 13:33:16 +0200

    Reduced number of test runs

--- test/channel-e2e-tests.sh
+++ test/channel-e2e-tests.sh
@@ -3,7 +3,7 @@
 source $(dirname $0)/e2e-common.sh
 
 header "Running first-event-delay.sh script ..."
-$(dirname $0)/scripts/first-event-delay.sh || fail_test "Failed"
+$(dirname $0)/scripts/first-event-delay.sh 25 || fail_test "Failed"
 
 header "Running e2e_channel go tests ..."
 go_test_e2e -timeout=1h ./test/e2e_channel/... -channels=messaging.knative.dev/v1beta1:KafkaChannel || fail_test "E2E suite (KafkaChannel) failed"
