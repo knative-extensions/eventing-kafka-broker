4184b224cf64f29705ccc31a0fd67250dbb9a878
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-07-05 13:11:23 +0200

    Add default retry delay to channel controller
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- data-plane/dispatcher/src/test/java/dev/knative/eventing/kafka/broker/dispatcher/impl/http/WebClientCloudEventSenderTest.java
+++ data-plane/dispatcher/src/test/java/dev/knative/eventing/kafka/broker/dispatcher/impl/http/WebClientCloudEventSenderTest.java
@@ -233,11 +233,11 @@ public class WebClientCloudEventSenderTest {
       .onSuccess(v -> success.set(true));
 
     await().untilFalse(success);
-    await().untilAdder(counter, is(equalTo(6L)));
+    await().untilAdder(counter, is(equalTo(retry+1)));
 
     // Verify that after some time counter is still equal to 6.
     Thread.sleep(10000L);
-    await().untilAdder(counter, is(equalTo(6L)));
+    await().untilAdder(counter, is(equalTo(retry+1)));
 
     sender.close().onSuccess(v -> context.completeNow());
   }
