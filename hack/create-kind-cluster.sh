5a2c9779086027a16606a8ec00fc8a29d50b45d1
Dirk Haubenreisser <haubenr@de.ibm.com>
2022-07-22 02:08:15 +0200

    Reduce KinD cluster memory footprint
    Signed-off-by: Dirk Haubenreisser <haubenr@de.ibm.com>

--- hack/create-kind-cluster.sh
+++ hack/create-kind-cluster.sh
@@ -58,8 +58,6 @@ kubeadmConfigPatches:
 nodes:
 - role: control-plane
   image: kindest/node:${NODE_VERSION}@${NODE_SHA}
-- role: worker
-  image: kindest/node:${NODE_VERSION}@${NODE_SHA}
 EOF
 
 # connect the registry to the cluster network if not already connected
