9297162769ccf331f32fd167120f056a42843c3b
Matthias Wessendorf <mwessend@redhat.com>
2022-09-06 13:48:15 +0200

    Instead of string we use a byte array for the response body
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- data-plane/dispatcher/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/impl/RecordDispatcherImpl.java
+++ data-plane/dispatcher/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/impl/RecordDispatcherImpl.java
@@ -34,6 +34,7 @@ import io.vertx.core.Context;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.kafka.client.common.tracing.ConsumerTracer;
 import io.vertx.kafka.client.consumer.KafkaConsumerRecord;
@@ -291,12 +292,13 @@ public class RecordDispatcherImpl implements RecordDispatcher {
     extensions.put(KN_ERROR_DEST_EXT_NAME, destination);
     extensions.put(KN_ERROR_CODE_EXT_NAME, String.valueOf(response.statusCode()));
 
-    var data = response.bodyAsString();
+    // we extract the response as byte array as we do not need a string
+    // representation of it
+    var data = response.bodyAsBuffer();
     if (data != null) {
       if (data.length() > KN_ERROR_DATA_MAX_BYTES) {
-        data = data.substring(0, KN_ERROR_DATA_MAX_BYTES);
+        data = Buffer.buffer(data.getBytes(0, KN_ERROR_DATA_MAX_BYTES));
       }
-
       extensions.put(KN_ERROR_DATA_EXT_NAME, Base64.getEncoder().encodeToString(data.getBytes()));
     }
 
