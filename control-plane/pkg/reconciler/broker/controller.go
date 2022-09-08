bcc970114c543ad921f99722d73d3179b6f82351
Matthias Wessendorf <mwessend@redhat.com>
2022-09-08 14:51:20 +0200

    Adding some retries
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- control-plane/pkg/reconciler/broker/controller.go
+++ control-plane/pkg/reconciler/broker/controller.go
@@ -71,6 +71,7 @@ func NewController(ctx context.Context, watcher configmap.Watcher, env *config.E
 		NewKafkaClusterAdminClient: sarama.NewClusterAdmin,
 		ConfigMapLister:            configmapInformer.Lister(),
 		Env:                        env,
+		Counter:                    NewCounter(),
 	}
 
 	logger := logging.FromContext(ctx)
