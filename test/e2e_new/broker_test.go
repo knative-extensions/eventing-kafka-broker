742249c8bf4b8f12dcfd9c82866f7eeea7452a39
Matthias Wessendorf <mwessend@redhat.com>
2022-09-12 09:35:05 +0200

    Fix rebase error
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- test/e2e_new/broker_test.go
+++ test/e2e_new/broker_test.go
@@ -72,20 +72,6 @@ func TestBrokerConfigMapDoesNotExist(t *testing.T) {
 	env.Test(ctx, t, features.BrokerConfigMapDoesNotExist())
 }
 
-func TestTriggerLatestOffset(t *testing.T) {
-	t.Parallel()
-
-	ctx, env := global.Environment(
-		knative.WithKnativeNamespace(system.Namespace()),
-		knative.WithLoggingConfig,
-		knative.WithTracingConfig,
-		k8s.WithEventListener,
-		environment.Managed(t),
-	)
-
-	env.Test(ctx, t, features.TriggerLatestOffset())
-}
-
 func TestBrokerCannotReachKafkaCluster(t *testing.T) {
 	t.Parallel()
 
