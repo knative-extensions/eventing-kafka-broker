3d068f2956e3497429847fcb2440ff62c3a3a294
Ansu Varghese <avarghese@us.ibm.com>
2022-11-23 01:01:33 -0500

    Update control-plane/pkg/autoscaler/keda/keda.go
    Co-authored-by: Pierangelo Di Pilato <pierangelodipilato@gmail.com>
    
    Signed-off-by: aavarghese <avarghese@us.ibm.com>

--- control-plane/pkg/autoscaler/keda/keda.go
+++ control-plane/pkg/autoscaler/keda/keda.go
@@ -22,6 +22,7 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
 	"knative.dev/eventing-kafka/pkg/apis/bindings/v1beta1"
 	"knative.dev/pkg/kmeta"
 
@@ -210,14 +211,14 @@ func retrieveSaslTypeIfPresent(cg *kafkainternals.ConsumerGroup, secret corev1.S
 		secretKeyRefKey := cg.Spec.Template.Spec.Auth.NetSpec.SASL.Type.SecretKeyRef.Key
 		if saslTypeValue, ok := secret.Data[secretKeyRefKey]; ok {
 			saslType := string(saslTypeValue)
-			return &saslType
+			return pointer.String(saslType)
 		}
 	}
 
 	if cg.Spec.Template.Spec.Auth.SecretSpec != nil && cg.Spec.Template.Spec.Auth.SecretSpec.Ref != nil {
 		if saslTypeValue, ok := secret.Data[security.SaslType]; ok {
 			saslType := string(saslTypeValue)
-			return &saslType
+			return pointer.String(saslType)
 		}
 	}
 	return nil
