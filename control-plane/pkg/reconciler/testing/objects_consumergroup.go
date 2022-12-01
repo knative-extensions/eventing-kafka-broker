398851e010f2a481cbbd6854fe3b54b5d345de69
Ali Ok <aliok@redhat.com>
2022-12-01 10:35:05 +0300

    Goimports

--- control-plane/pkg/reconciler/testing/objects_consumergroup.go
+++ control-plane/pkg/reconciler/testing/objects_consumergroup.go
@@ -17,8 +17,9 @@
 package testing
 
 import (
-	reconcilertesting "knative.dev/eventing/pkg/reconciler/testing/v1"
 	"time"
+
+	reconcilertesting "knative.dev/eventing/pkg/reconciler/testing/v1"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
