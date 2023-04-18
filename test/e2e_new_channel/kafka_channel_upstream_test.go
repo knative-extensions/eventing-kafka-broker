c242548866f3056dee1f925802235e70fe04b4f0
Matthias Wessendorf <mwessend@redhat.com>
2023-04-18 14:55:06 +0200

    unskip
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- test/e2e_new_channel/kafka_channel_upstream_test.go
+++ test/e2e_new_channel/kafka_channel_upstream_test.go
@@ -212,7 +212,6 @@ the dead letter sink.
 It uses Subscription's spec.reply as spec.subscriber.
 */
 func TestChannelDeadLetterSink(t *testing.T) {
-	t.Skip("tmp")
 	t.Parallel()
 
 	ctx, env := global.Environment(
