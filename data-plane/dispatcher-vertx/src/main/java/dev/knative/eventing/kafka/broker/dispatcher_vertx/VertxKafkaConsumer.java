26ee8955acb15cda96cc883fd67718b21662ac42
debasishbsws <debasishbsws.abc@gmail.com>
2023-05-26 17:21:15 +0530

    adding modules
    Signed-off-by: debasishbsws <debasishbsws.abc@gmail.com>

---
+++ data-plane/dispatcher-vertx/src/main/java/dev/knative/eventing/kafka/broker/dispatcher_vertx/VertxKafkaConsumer.java
@@ -0,0 +1,7 @@
+package dev.knative.eventing.kafka.broker.dispatcher_vertx;
+
+public class VertxKafkaConsumer {
+    public static void main(String[] args) {
+        System.out.println("Hello Vertx Kafka!");
+    }
+}
---
+++ data-plane/receiver-vertx/src/main/java/dev/knative/eventing/kafka/broker/receiver_vertx/VertxKafkaProducer.java
@@ -0,0 +1,7 @@
+package dev.knative.eventing.kafka.broker.receiver_vertx;
+
+public class VertxKafkaProducer {
+    public static void main(String[] args) {
+        System.out.println("Hello Vertx Kafka!");
+    }
+}
