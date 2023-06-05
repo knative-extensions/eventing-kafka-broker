fc15f0d91c3b23a1cc9a482b2848f9e547d123f6
Martin Gencur <mgencur@redhat.com>
2023-06-05 16:01:42 +0200

    Conformance tests against MTChannelBasedBroker (#3008)
    * Conformance tests against MTChannelBasedBroker
    
    * Install specific config map only for MTChannelBrokerClassValue
    
    * Allow any broker class
    
    * Move broker_conformance_test back under e2e_new

--- test/e2e_new/broker_conformance_test.go
+++ test/e2e_new/broker_conformance_test.go
@@ -24,10 +24,11 @@ import (
 
 	// Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-
 	"knative.dev/pkg/system"
 	_ "knative.dev/pkg/system/testing"
 
+	brokerconfigmap "knative.dev/eventing-kafka-broker/test/rekt/resources/configmap/broker"
+	"knative.dev/eventing/pkg/apis/eventing"
 	"knative.dev/eventing/test/rekt/features/broker"
 	b "knative.dev/eventing/test/rekt/resources/broker"
 	"knative.dev/reconciler-test/pkg/environment"
@@ -45,10 +46,28 @@ func TestBrokerConformance(t *testing.T) {
 		environment.Managed(t),
 	)
 
+	opts := b.WithEnvConfig()
+
+	if b.EnvCfg.BrokerClass == eventing.MTChannelBrokerClassValue {
+		configName := feature.MakeRandomK8sName("kafka-broker-config")
+		opts = append(opts, b.WithConfig(configName))
+		env.Prerequisite(ctx, t, BrokerCreateConfigMap(configName))
+	}
+
 	brokerName := feature.MakeRandomK8sName("broker")
 
-	env.Prerequisite(ctx, t, broker.GoesReady(brokerName, b.WithEnvConfig()...))
+	env.Prerequisite(ctx, t, broker.GoesReady(brokerName, opts...))
 
-	env.TestSet(ctx, t, broker.ControlPlaneConformance(brokerName, b.WithEnvConfig()...))
+	env.TestSet(ctx, t, broker.ControlPlaneConformance(brokerName, opts...))
 	env.TestSet(ctx, t, broker.DataPlaneConformance(brokerName))
+}
+
+func BrokerCreateConfigMap(configName string) *feature.Feature {
+	f := feature.NewFeature()
+
+	f.Setup("create broker config", brokerconfigmap.Install(configName,
+		brokerconfigmap.WithKafkaChannelMTBroker(),
+	))
+
+	return f
 }
