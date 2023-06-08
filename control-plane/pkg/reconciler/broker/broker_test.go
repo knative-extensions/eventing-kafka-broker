a7b1dbed42185b701cd93074c03e2f5192637aff
Calum Murray <cmurray@redhat.com>
2023-06-08 16:30:36 -0400

    Updated tests to use certs from eventingtlstesting

--- control-plane/pkg/reconciler/broker/broker_test.go
+++ control-plane/pkg/reconciler/broker/broker_test.go
@@ -44,6 +44,7 @@ import (
 	clientgotesting "k8s.io/client-go/testing"
 	eventing "knative.dev/eventing/pkg/apis/eventing/v1"
 	"knative.dev/eventing/pkg/apis/feature"
+	"knative.dev/eventing/pkg/eventingtls/eventingtlstesting"
 	"knative.dev/pkg/apis"
 	kubeclient "knative.dev/pkg/client/injection/kube/client/fake"
 	"knative.dev/pkg/controller"
@@ -78,30 +79,6 @@ const (
 
 	bootstrapServers = "kafka-1:9092,kafka-2:9093"
 
-	testCaCerts = `
------BEGIN CERTIFICATE-----
-MIIDmjCCAoKgAwIBAgIUYzA4bTMXevuk3pl2Mn8hpCYL2C0wDQYJKoZIhvcNAQEL
-BQAwLzELMAkGA1UEBhMCVVMxIDAeBgNVBAMMF0tuYXRpdmUtRXhhbXBsZS1Sb290
-LUNBMB4XDTIzMDQwNTEzMTUyNFoXDTI2MDEyMzEzMTUyNFowbTELMAkGA1UEBhMC
-VVMxEjAQBgNVBAgMCVlvdXJTdGF0ZTERMA8GA1UEBwwIWW91ckNpdHkxHTAbBgNV
-BAoMFEV4YW1wbGUtQ2VydGlmaWNhdGVzMRgwFgYDVQQDDA9sb2NhbGhvc3QubG9j
-YWwwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC5teo+En6U5nhqn7Sc
-uanqswUmPlgs9j/8l21Rhb4T+ezlYKGQGhbJyFFMuiCE1Rjn8bpCwi7Nnv12Y2nz
-FhEv2Jx0yL3Tqx0Q593myqKDq7326EtbO7wmDT0XD03twH5i9XZ0L0ihPWn1mjUy
-WxhnHhoFpXrsnQECJorZY6aTrFbGVYelIaj5AriwiqyL0fET8pueI2GwLjgWHFSH
-X8XsGAlcLUhkQG0Z+VO9usy4M1Wpt+cL6cnTiQ+sRmZ6uvaj8fKOT1Slk/oUeAi4
-WqFkChGzGzLik0QrhKGTdw3uUvI1F2sdQj0GYzXaWqRz+tP9qnXdzk1GrszKKSlm
-WBTLAgMBAAGjcDBuMB8GA1UdIwQYMBaAFJJcCftus4vj98N0zQQautsjEu82MAkG
-A1UdEwQCMAAwCwYDVR0PBAQDAgTwMBQGA1UdEQQNMAuCCWxvY2FsaG9zdDAdBgNV
-HQ4EFgQUnu/3vqA3VEzm128x/hLyZzR9JlgwDQYJKoZIhvcNAQELBQADggEBAFc+
-1cKt/CNjHXUsirgEhry2Mm96R6Yxuq//mP2+SEjdab+FaXPZkjHx118u3PPX5uTh
-gTT7rMfka6J5xzzQNqJbRMgNpdEFH1bbc11aYuhi0khOAe0cpQDtktyuDJQMMv3/
-3wu6rLr6fmENo0gdcyUY9EiYrglWGtdXhlo4ySRY8UZkUScG2upvyOhHTxVCAjhP
-efbMkNjmDuZOMK+wqanqr5YV6zMPzkQK7DspfRgasMAQmugQu7r2MZpXg8Ilhro1
-s/wImGnMVk5RzpBVrq2VB9SkX/ThTVYEC/Sd9BQM364MCR+TA1l8/ptaLFLuwyw8
-O2dgzikq8iSy1BlRsVw=
------END CERTIFICATE-----
-`
 	brokerIngressTLSSecretName = "kafka-broker-ingress-server-tls"
 )
 
@@ -2701,7 +2678,7 @@ func brokerFinalization(t *testing.T, format string, env config.Env) {
 							{
 								Name:    pointer.String("https"),
 								URL:     httpsURL(BrokerName, BrokerNamespace),
-								CACerts: pointer.String(testCaCerts),
+								CACerts: pointer.String(string(eventingtlstesting.CA)),
 							},
 						}),
 						WithBrokerAddress(duckv1.Addressable{
@@ -2767,13 +2744,13 @@ func brokerFinalization(t *testing.T, format string, env config.Env) {
 							{
 								Name:    pointer.String("https"),
 								URL:     httpsURL(BrokerName, BrokerNamespace),
-								CACerts: pointer.String(testCaCerts),
+								CACerts: pointer.String(string(eventingtlstesting.CA)),
 							},
 						}),
 						WithBrokerAddress(duckv1.Addressable{
 							Name:    pointer.String("https"),
 							URL:     httpsURL(BrokerName, BrokerNamespace),
-							CACerts: pointer.String(testCaCerts),
+							CACerts: pointer.String(string(eventingtlstesting.CA)),
 						}),
 					),
 				},
@@ -2893,7 +2870,7 @@ func makeTLSSecret() *corev1.Secret {
 			Name:      brokerIngressTLSSecretName,
 		},
 		Data: map[string][]byte{
-			"ca.crt": []byte(testCaCerts),
+			"ca.crt": []byte(eventingtlstesting.CA),
 		},
 		Type: corev1.SecretTypeTLS,
 	}
