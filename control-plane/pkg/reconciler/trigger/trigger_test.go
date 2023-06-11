c9a44c4c45758b98cb8776f1baa5246d040c10d3
Vishal Choudhary <sendtovishalchoudhary@gmail.com>
2023-06-11 14:26:06 +0530

    tests
    Signed-off-by: Vishal Choudhary <sendtovishalchoudhary@gmail.com>

--- control-plane/pkg/reconciler/trigger/trigger_test.go
+++ control-plane/pkg/reconciler/trigger/trigger_test.go
@@ -43,9 +43,11 @@ import (
 
 	eventingduck "knative.dev/eventing/pkg/apis/duck/v1"
 	eventing "knative.dev/eventing/pkg/apis/eventing/v1"
+	v1 "knative.dev/eventing/pkg/apis/eventing/v1"
 	"knative.dev/eventing/pkg/apis/feature"
 	eventingclient "knative.dev/eventing/pkg/client/injection/client/fake"
 	triggerreconciler "knative.dev/eventing/pkg/client/injection/reconciler/eventing/v1/trigger"
+	"knative.dev/eventing/pkg/eventingtls/eventingtlstesting"
 	reconcilertesting "knative.dev/eventing/pkg/reconciler/testing/v1"
 
 	"knative.dev/eventing-kafka-broker/control-plane/pkg/config"
@@ -2825,6 +2827,85 @@ func triggerFinalizer(t *testing.T, format string, env config.Env) {
 				},
 			},
 		},
+		{
+			Name: "Reconciled normal - With Subscriber CA Cert",
+			Objects: []runtime.Object{
+				NewBroker(
+					BrokerReady,
+					WithTopicStatusAnnotation(BrokerTopic()),
+					WithBootstrapServerStatusAnnotation(bootstrapServers),
+				),
+				newTriggerWithCert(withDelivery),
+				NewService(),
+				NewConfigMapFromContract(&contract.Contract{
+					Resources: []*contract.Resource{
+						{
+							Uid:     BrokerUUID,
+							Topics:  []string{BrokerTopic()},
+							Ingress: &contract.Ingress{Path: receiver.Path(BrokerNamespace, BrokerName)},
+						},
+					},
+				}, env.DataPlaneConfigMapNamespace, env.ContractConfigMapName, env.ContractConfigMapFormat),
+				BrokerDispatcherPod(env.SystemNamespace, nil),
+				DataPlaneConfigMap(env.DataPlaneConfigMapNamespace, env.DataPlaneConfigConfigMapName, brokerreconciler.ConsumerConfigKey,
+					DataPlaneConfigInitialOffset(brokerreconciler.ConsumerConfigKey, sources.OffsetLatest),
+				),
+			},
+			Key: testKey,
+			WantEvents: []string{
+				finalizerUpdatedEvent,
+			},
+			WantPatches: []clientgotesting.PatchActionImpl{
+				patchFinalizers(),
+			},
+			WantUpdates: []clientgotesting.UpdateActionImpl{
+				ConfigMapUpdate(env.DataPlaneConfigMapNamespace, env.ContractConfigMapName, env.ContractConfigMapFormat, &contract.Contract{
+					Resources: []*contract.Resource{
+						{
+							Uid:     BrokerUUID,
+							Topics:  []string{BrokerTopic()},
+							Ingress: &contract.Ingress{Path: receiver.Path(BrokerNamespace, BrokerName)},
+							Egresses: []*contract.Egress{
+								{
+									Destination:        ServiceURL,
+									DestinationCACerts: string(eventingtlstesting.CA),
+									ConsumerGroup:      TriggerUUID,
+									Uid:                TriggerUUID,
+									Reference:          TriggerReference(),
+									EgressConfig: &contract.EgressConfig{
+										DeadLetter:    url.String(),
+										Retry:         3,
+										BackoffPolicy: contract.BackoffPolicy_Exponential,
+										BackoffDelay:  uint64(time.Second.Milliseconds()),
+										Timeout:       uint64((time.Second * 2).Milliseconds()),
+									},
+								},
+							},
+						},
+					},
+					Generation: 1,
+				}),
+				BrokerDispatcherPodUpdate(env.SystemNamespace, map[string]string{
+					base.VolumeGenerationAnnotationKey: "1",
+				}),
+			},
+			WantStatusUpdates: []clientgotesting.UpdateActionImpl{
+				{
+					Object: newTriggerWithCert(
+						withDelivery,
+						reconcilertesting.WithInitTriggerConditions,
+						reconcilertesting.WithTriggerSubscribed(),
+						withSubscriberURI,
+						reconcilertesting.WithTriggerDependencyReady(),
+						reconcilertesting.WithTriggerBrokerReady(),
+						withTriggerSubscriberResolvedSucceeded(contract.DeliveryOrder_UNORDERED),
+						withTriggerStatusGroupIdAnnotation(TriggerUUID),
+						reconcilertesting.WithTriggerDeadLetterSinkResolvedSucceeded(),
+						reconcilertesting.WithTriggerStatusDeadLetterSinkURI("http://localhost/path"),
+					),
+				},
+			},
+		},
 	}
 
 	useTable(t, table, &env)
@@ -2924,6 +3005,31 @@ func newTrigger(options ...reconcilertesting.TriggerOption) runtime.Object {
 			},
 		)...,
 	)
+}
+
+func newTriggerWithCert(options ...reconcilertesting.TriggerOption) runtime.Object {
+	return reconcilertesting.NewTrigger(
+		TriggerName,
+		TriggerNamespace,
+		BrokerName,
+		append(
+			options,
+			WithTriggerSubscriberURIAndCert(ServiceURL),
+			func(t *eventing.Trigger) {
+				t.UID = TriggerUUID
+			},
+		)...,
+	)
+}
+
+func WithTriggerSubscriberURIAndCert(rawurl string) reconcilertesting.TriggerOption {
+	uri, _ := apis.ParseURL(rawurl)
+	return func(t *v1.Trigger) {
+		t.Spec.Subscriber = duckv1.Destination{
+			URI:     uri,
+			CACerts: pointer.String(string(eventingtlstesting.CA)),
+		}
+	}
 }
 
 func withFilters(attributes eventing.TriggerFilterAttributes, newFilters []eventing.SubscriptionsAPIFilter, withNewFilters bool) func(*eventing.Trigger) {
