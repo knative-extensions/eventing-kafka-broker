80d3cf22740b56c2c2fe7b0e16ac33270100e099
Debasish Biswas <debasishbsws.abc@gmail.com>
2023-06-26 21:21:53 +0530

    `ReactiveKafkaConsumer` Interface and it's Vert.x Implementation (#3159)
    * Identified all consumer function
    
    * VertxConsumer Implemtation
    
    * Add Licence header.
    
    * Just a bit for formating.
    
    * remove` assign` and add `commit` method
    
    * refactor, Use java streamAPI
    
    * Update data-plane/dispatcher/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/ReactiveKafkaConsumer.java
    
    Co-authored-by: Pierangelo Di Pilato <pierangelodipilato@gmail.com>
    
    ---------
    
    Co-authored-by: Pierangelo Di Pilato <pierangelodipilato@gmail.com>

--- data-plane/dispatcher-vertx/src/main/java/dev/knative/eventing/kafka/broker/dispatchervertx/VertxKafkaConsumer.java
+++ data-plane/dispatcher-vertx/src/main/java/dev/knative/eventing/kafka/broker/dispatchervertx/VertxKafkaConsumer.java
@@ -15,8 +15,90 @@
  */
 package dev.knative.eventing.kafka.broker.dispatchervertx;
 
-public class VertxKafkaConsumer {
-    public static void main(String[] args) {
-        System.out.println("Hello Vertx KafkaConsumer!");
+import java.time.Duration;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+import dev.knative.eventing.kafka.broker.dispatcher.ReactiveKafkaConsumer;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.kafka.client.common.KafkaClientOptions;
+import io.vertx.kafka.client.consumer.KafkaConsumer;
+
+public class VertxKafkaConsumer<K, V> implements ReactiveKafkaConsumer<K, V> {
+
+    private KafkaConsumer<K, V> consumer;
+
+    public VertxKafkaConsumer(Vertx v, KafkaClientOptions configs) {
+        consumer = KafkaConsumer.create(v, configs);
     }
+
+    @Override
+    public Future<Map<TopicPartition, OffsetAndMetadata>> commit(Map<TopicPartition, OffsetAndMetadata> offset) {
+
+        final var vertxOffset = offset.entrySet().stream()
+            .collect(Collectors.toMap(
+                entry -> new io.vertx.kafka.client.common.TopicPartition(entry.getKey().topic(),entry.getKey().partition()),
+                entry -> new io.vertx.kafka.client.consumer.OffsetAndMetadata(entry.getValue().offset(),entry.getValue().metadata())
+            ));
+
+        return consumer.commit(vertxOffset).map(vertxOffset.entrySet().stream()
+            .collect(Collectors.toMap(
+                entry -> new TopicPartition(entry.getKey().getTopic(), entry.getKey().getPartition()),
+                entry -> new OffsetAndMetadata(entry.getValue().getOffset(), entry.getValue().getMetadata())
+            ))
+        );
+    }
+
+    @Override
+    public Future<Void> close() {
+        return consumer.close();
+    }
+
+    @Override
+    public Future<Void> pause(Collection<TopicPartition> partitions) {
+        var vertxTopicPartitions = partitions.stream()
+            .map(topicPartition -> new io.vertx.kafka.client.common.TopicPartition(topicPartition.topic(),topicPartition.partition()))
+            .collect(Collectors.toSet());
+
+        return consumer.pause(vertxTopicPartitions);
+    }
+
+    @Override
+    public Future<ConsumerRecords<K, V>> poll(Duration timeout) {
+        return consumer.poll(timeout).map(kafkaConsumerRecords -> kafkaConsumerRecords.records());
+    }
+
+    @Override
+    public Future<Void> resume(Collection<TopicPartition> partitions) {
+        var vertxTopicPartitions = partitions.stream()
+            .map(topicPartition -> new io.vertx.kafka.client.common.TopicPartition(topicPartition.topic(),topicPartition.partition()))
+            .collect(Collectors.toSet());
+
+        return consumer.resume(vertxTopicPartitions);
+    }
+
+    @Override
+    public Future<Void> subscribe(Collection<String> topics) {
+        return consumer.subscribe(new HashSet<>(topics));
+    }
+
+    @Override
+    public Consumer<K, V> unwrap() {
+        return consumer.unwrap();
+    }
+
+    @Override
+    public ReactiveKafkaConsumer<K, V> exceptionHandler(Handler<Throwable> handler) {
+        consumer = consumer.exceptionHandler(handler);
+        return this;
+    }
+
 }
