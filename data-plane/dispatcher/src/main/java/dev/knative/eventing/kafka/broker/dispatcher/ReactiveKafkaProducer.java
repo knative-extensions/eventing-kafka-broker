27fd8e719d9b8e95842a86f892ecfe13c40cc2af
debasishbsws <debasishbsws.abc@gmail.com>
2023-07-04 17:27:57 +0530

    licence header added

--- data-plane/dispatcher/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/ReactiveKafkaProducer.java
+++ data-plane/dispatcher/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/ReactiveKafkaProducer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2018 Knative Authors (knative-dev@googlegroups.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.knative.eventing.kafka.broker.dispatcher;
 
 import org.apache.kafka.clients.producer.Producer;
@@ -23,16 +38,6 @@ public interface ReactiveKafkaProducer<K, V> {
      * @return a future notifying when the producer is closed
      */
     Future<Void> close();
-
-    // /**
-    //  * Flush the producer
-    //  * <p>
-    //  * This method ensures all records stored in the producer buffer are actually
-    //  * sent to the Kafka cluster.
-    //  *
-    //  * @return a future notifying when the producer is flushed
-    //  */
-    // Future<Void> flush();
 
     /**
      * Wnrap the underlying Kafka producer
