d8e29080d134f305a6fc4639cfded099b3cc8fe9
Calum Murray <cmurray@redhat.com>
2023-07-28 12:09:33 -0400

    Fixed junit being used for test
    Signed-off-by: Calum Murray <cmurray@redhat.com>

--- data-plane/core/src/test/java/dev/knative/eventing/kafka/broker/core/eventtype/EventTypeTest.java
+++ data-plane/core/src/test/java/dev/knative/eventing/kafka/broker/core/eventtype/EventTypeTest.java
@@ -18,40 +18,34 @@ package dev.knative.eventing.kafka.broker.core.eventtype;
 
 import io.fabric8.kubernetes.api.model.KubernetesResourceList;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.dsl.MixedOperation;
-import io.fabric8.kubernetes.client.dsl.Resource;
 import io.fabric8.kubernetes.client.server.mock.EnableKubernetesMockClient;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import io.fabric8.kubernetes.client.server.mock.KubernetesMockServer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 @EnableKubernetesMockClient(crud = true)
 public class EventTypeTest {
 
-    static KubernetesClient client;
-    MixedOperation<EventType, KubernetesResourceList<EventType>, Resource<EventType>> eventTypeClient;
+  private KubernetesClient kubernetesClient;
+  private KubernetesMockServer server;
 
-    @Test
-    public void testCreateEventType() {
-        eventTypeClient
-                .resource(new EventTypeBuilder()
-                        .withReference(new KReference("eventing.knative.dev/v1", "KafkaBroker", "MyBroker", "default"))
-                        .withSchema("sample schema")
-                        .withSchemaDescription("sample schema description")
-                        .withDescription("a sample event type")
-                        .withName("sample.event.type")
-                        .withNamespace("default")
-                        .build())
-                .create();
+  @Test
+  public void testCreateEventType() {
+    var eventTypeClient = kubernetesClient.resources(EventType.class);
+    eventTypeClient
+      .resource(new EventTypeBuilder()
+        .withReference(new KReference("eventing.knative.dev/v1", "KafkaBroker", "MyBroker", "default"))
+        .withSchema("sample schema")
+        .withSchemaDescription("sample schema description")
+        .withDescription("a sample event type")
+        .withName("sample.event.type")
+        .withNamespace("default")
+        .build())
+      .create();
 
-        KubernetesResourceList<EventType> eventTypeList =
-                eventTypeClient.inNamespace("default").list();
-        Assert.assertNotNull(eventTypeList);
-        Assert.assertEquals(1, eventTypeList.getItems().size());
-    }
-
-    @Before
-    public void setupEventTypeClient() {
-        this.eventTypeClient = client.resources(EventType.class);
-    }
+    KubernetesResourceList<EventType> eventTypeList =
+      eventTypeClient.inNamespace("default").list();
+    Assertions.assertNotNull(eventTypeList);
+    Assertions.assertEquals(1, eventTypeList.getItems().size());
+  }
 }
