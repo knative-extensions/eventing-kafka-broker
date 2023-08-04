31b59c64219e4fcab997ee3c09161e4eb1be050e
debasishbsws <debasishbsws.dev@gmail.com>
2023-08-04 13:30:04 +0530

    replace with knative licence header
    Signed-off-by: debasishbsws <debasishbsws.abc@gmail.com>
    Signed-off-by: debasishbsws <debasishbsws.dev@gmail.com>

--- data-plane/core/src/main/java/dev/knative/eventing/kafka/broker/core/tracing/kafka/ConsumerTracer.java
+++ data-plane/core/src/main/java/dev/knative/eventing/kafka/broker/core/tracing/kafka/ConsumerTracer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Red Hat Inc.
+ * Copyright © 2018 Knative Authors (knative-dev@googlegroups.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
