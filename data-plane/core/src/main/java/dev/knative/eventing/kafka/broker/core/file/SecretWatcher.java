59acf2f83475aadc6804036a3aebbb411626f3c9
Leo Li <leoli@redhat.com>
2023-09-08 03:19:37 -0400

    Modify the secretWatcher implementation

--- data-plane/core/src/main/java/dev/knative/eventing/kafka/broker/core/file/SecretWatcher.java
+++ data-plane/core/src/main/java/dev/knative/eventing/kafka/broker/core/file/SecretWatcher.java
@@ -17,6 +17,7 @@ package dev.knative.eventing.kafka.broker.core.file;
 
 import java.io.IOException;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
@@ -27,52 +28,50 @@ import org.slf4j.LoggerFactory;
 
 /** Watches a directory for changes to TLS secrets. */
 public class SecretWatcher implements Runnable {
-    private static final Logger logger = LoggerFactory.getLogger(SecretWatcher.class);
+  private static final Logger logger = LoggerFactory.getLogger(SecretWatcher.class);
 
-    private final String dir; // directory to watch
-    private final WatchService watcher; // watch service
-    private final Runnable updateAction; // action to run when a change is detected
+  private final String dir; // directory to watch
+  private final WatchService watcher; // watch service
+  private final Runnable updateAction; // action to run when a change is detected
 
-    private static String KEY_FILE = "tls.key";
-    private static String CRT_FILE = "tls.crt";
+  public SecretWatcher(String dir, Runnable updateAction) throws IOException {
+    this.dir = dir;
+    this.updateAction = updateAction;
+    this.watcher = FileSystems.getDefault().newWatchService();
 
-    public SecretWatcher(String dir, Runnable updateAction) throws IOException {
-        this.dir = dir;
-        this.updateAction = updateAction;
-        this.watcher = FileSystems.getDefault().newWatchService();
+    Path path = Path.of(dir);
+    path.register(watcher, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE);
+  }
 
-        Path path = Path.of(dir);
-        path.register(watcher, StandardWatchEventKinds.ENTRY_MODIFY);
-    }
-
-    @Override
-    public void run() {
-        try {
-            WatchKey key;
-            while ((key = watcher.take()) != null) {
-                for (WatchEvent<?> event : key.pollEvents()) {
-                    Path changed = (Path) event.context();
-                    if (changed.endsWith(KEY_FILE) || changed.endsWith(CRT_FILE)) {
-                        logger.debug("Detected change to secret {}", changed);
-                        updateAction.run();
-                    }
-                }
-                key.reset();
-            }
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            logger.error("Watcher exception", e);
-        } finally {
-            this.stop();
+  @Override
+  public void run() {
+    try {
+      WatchKey key;
+      while ((key = watcher.take()) != null) {
+        for (WatchEvent<?> event : key.pollEvents()) {
+          Path changed = (Path) event.context();
+          Path fullChangedPath = Path.of(dir, changed.toString());
+          if (Files.isSymbolicLink(fullChangedPath)) {
+            logger.info("Detected change to secret {}", changed);
+            updateAction.run();
+          }
         }
+        key.reset();
+      }
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      logger.info("Watcher exception", e);
+    } finally {
+      this.stop();
     }
+  }
 
-    // stop the watcher
-    public void stop() {
-        try {
-            watcher.close();
-        } catch (IOException e) {
-            logger.error("Failed to close secret watcher", e);
-        }
+  // stop the watcher
+  public void stop() {
+    try {
+      watcher.close();
+    } catch (IOException e) {
+      logger.error("Failed to close secret watcher", e);
     }
+  }
 }
