a48b39622fcd42ce10a0259be9d0b04d0a00fe93
Calum Murray <cmurray@redhat.com>
2023-09-29 13:20:41 -0400

    Spotless
    Signed-off-by: Calum Murray <cmurray@redhat.com>

--- data-plane/benchmarks/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/impl/filter/SampleEvent.java
+++ data-plane/benchmarks/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/impl/filter/SampleEvent.java
@@ -21,25 +21,25 @@ import io.cloudevents.core.builder.CloudEventBuilder;
 import java.net.URI;
 
 public class SampleEvent {
-  public static CloudEvent event() {
-    return CloudEventBuilder.v1()
-        .withId("abcdefghijklmnop")
-        .withSource(URI.create("http://localhost"))
-        .withType("com.github.pull.create")
-        .withDataSchema(URI.create("/api/schema"))
-        .withDataContentType("testContentType")
-        .withSubject("testSubject")
-        .build();
-  }
+    public static CloudEvent event() {
+        return CloudEventBuilder.v1()
+                .withId("abcdefghijklmnop")
+                .withSource(URI.create("http://localhost"))
+                .withType("com.github.pull.create")
+                .withDataSchema(URI.create("/api/schema"))
+                .withDataContentType("testContentType")
+                .withSubject("testSubject")
+                .build();
+    }
 
-  public static CloudEvent otherEvent() {
-    return CloudEventBuilder.v1()
-        .withId("abcdefghijklmnop")
-        .withSource(URI.create("http://localhost"))
-        .withType("other.event.type")
-        .withDataSchema(URI.create("/api/schema"))
-        .withDataContentType("testContentType")
-        .withSubject("testSubject")
-        .build();
-  }
+    public static CloudEvent otherEvent() {
+        return CloudEventBuilder.v1()
+                .withId("abcdefghijklmnop")
+                .withSource(URI.create("http://localhost"))
+                .withType("other.event.type")
+                .withDataSchema(URI.create("/api/schema"))
+                .withDataContentType("testContentType")
+                .withSubject("testSubject")
+                .build();
+    }
 }
