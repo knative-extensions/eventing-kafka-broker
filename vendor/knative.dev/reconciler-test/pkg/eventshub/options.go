5857c11118fad33f76545d2672377b6282c493c1
Knative Automation <automation@knative.team>
2023-10-24 02:04:03 +0000

    upgrade to latest dependencies
    bumping knative.dev/eventing 16d75a9...b5fd264:
      > b5fd264 Shell executor logs through testing.T in upgrade tests (# 7367)
      > 5848584 [main] Upgrade to latest dependencies (# 7388)
      > 16a3986 Don't override default values when applying partial features.yaml configmap  (# 7379)
      > 4d14c21 Added test for multiple sinks, multiple triggers (# 7350)
      > a261e06 OIDC - Support auto generation of PingSource identity service account and expose in AuthStatus (# 7344)
    bumping knative.dev/pkg d6ab729...29775d7:
      > 29775d7 [release-1.12] [CVE-2023-44487] Disable http2 for webhooks (# 2876)
    bumping knative.dev/reconciler-test 317033b...e3a9c2e:
      > e3a9c2e Improve error message when deleting resources (# 617)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/reconciler-test/pkg/eventshub/options.go
+++ vendor/knative.dev/reconciler-test/pkg/eventshub/options.go
@@ -202,11 +202,6 @@ func DropEventsResponseHeaders(headers map[string]string) EventsHubOption {
 	)
 }
 
-// OIDCReceiverAudience sets the expected audience for received OIDC tokens on the receiver side
-func OIDCReceiverAudience(aud string) EventsHubOption {
-	return compose(envOption(OIDCReceiverAudienceEnv, aud), envOIDCEnabled())
-}
-
 // --- Sender options
 
 // InitialSenderDelay defines how much the sender has to wait (in millisecond), when started, before start sending events.
@@ -286,11 +281,6 @@ func OIDCExpiredToken() EventsHubOption {
 // OIDCInvalidAudience creates an OIDC token with an invalid audience
 func OIDCInvalidAudience() EventsHubOption {
 	return compose(envOption(OIDCGenerateInvalidAudienceTokenEnv, "true"), envOIDCEnabled())
-}
-
-// OIDCSinkAudience sets the Audience of the Sink
-func OIDCSinkAudience(aud string) EventsHubOption {
-	return oidcSinkAudience(&aud)
 }
 
 func oidcSinkAudience(aud *string) EventsHubOption {
