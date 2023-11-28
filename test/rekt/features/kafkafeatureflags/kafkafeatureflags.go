875a7aadb0a4cb5a0da80791cb4c0f9f20977935
Calum Murray <cmurray@redhat.com>
2023-11-28 14:40:21 -0500

    build: try again to fix lint issues
    Signed-off-by: Calum Murray <cmurray@redhat.com>

--- test/rekt/features/kafkafeatureflags/kafkafeatureflags.go
+++ test/rekt/features/kafkafeatureflags/kafkafeatureflags.go
@@ -17,34 +17,14 @@
 package kafkafeatureflags
 
 import (
+	"context"
 	"fmt"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"knative.dev/pkg/system"
-	"knative.dev/reconciler-test/pkg/feature"
-)
-
-/*
-Copyright 2023 The Knative Authors
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-import (
-	"context"
-
 	kafkafeature "knative.dev/eventing-kafka-broker/control-plane/pkg/apis/config"
 	kubeclient "knative.dev/pkg/client/injection/kube/client"
+	"knative.dev/pkg/system"
+	"knative.dev/reconciler-test/pkg/feature"
 )
 
 func SourceScalingEnabled() feature.ShouldRun {
