07aa0b6e4f989ab33200bb603777244e9b45475f
Calum Murray <cmurray@redhat.com>
2024-01-12 14:47:32 -0500

    remove unnecessary null event deserialization
    Signed-off-by: Calum Murray <cmurray@redhat.com>

--- data-plane/dispatcher/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/impl/consumer/CloudEventOverridesMutator.java
+++ data-plane/dispatcher/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/impl/consumer/CloudEventOverridesMutator.java
@@ -37,26 +37,10 @@ public class CloudEventOverridesMutator implements CloudEventMutator {
 
     @Override
     public CloudEvent apply(ConsumerRecord<Object, CloudEvent> record) {
-        final var cloudEvent = this.maybeDeserializeFromHeaders(record);
-        final var builder = CloudEventBuilder.from(cloudEvent);
+        final var builder = CloudEventBuilder.from(record.value());
         applyKafkaMetadata(builder, record.partition(), record.offset());
         applyCloudEventOverrides(builder);
         return builder.build();
-    }
-
-    private CloudEvent maybeDeserializeFromHeaders(ConsumerRecord<Object, CloudEvent> record) {
-        if (record.value() != null) {
-            return record.value();
-        }
-        // A valid CloudEvent in the CE binary protocol binding of Kafka
-        // might be composed by only Headers.
-        //
-        // KafkaConsumer doesn't call the deserializer if the value
-        // is null.
-        //
-        // That means that we get a record with a null value and some CE
-        // headers even though the record is a valid CloudEvent.
-        return cloudEventDeserializer.deserialize(record.topic(), record.headers(), null);
     }
 
     private void applyCloudEventOverrides(CloudEventBuilder builder) {
