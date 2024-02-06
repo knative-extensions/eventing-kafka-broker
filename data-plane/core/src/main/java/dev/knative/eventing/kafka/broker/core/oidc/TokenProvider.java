2c8b901e52fdfef816293e58ef271fe3c46f1e62
Leo Li <leoli@redhat.com>
2024-02-06 13:35:49 -0500

    [data-plane] Add the caching for OIDC JWT token to tokenprovider (#3663)
    * Add the caching to tokenprovider
    
    * Add the boiler
    
    * Running the codegen
    
    * Running the codegen
    
    * Revert "Running the codegen"
    
    This reverts commit 0ccf69c48e1592e88febdadd5d9451803adf64ce.
    
    * Use constant, set buffer before token expire, remove unnecessary change
    
    * Codegen changes
    
    * Codegen changes
    
    * Update data-plane/core/src/main/java/dev/knative/eventing/kafka/broker/core/oidc/TokenProvider.java
    
    Co-authored-by: Christoph Stäbler <cstabler@redhat.com>
    
    * Fix the review comments
    
    * Run codegen
    
    * Codegen changes
    
    * Code gen again
    
    ---------
    
    Co-authored-by: Christoph Stäbler <cstabler@redhat.com>

--- data-plane/core/src/main/java/dev/knative/eventing/kafka/broker/core/oidc/TokenProvider.java
+++ data-plane/core/src/main/java/dev/knative/eventing/kafka/broker/core/oidc/TokenProvider.java
@@ -15,6 +15,8 @@
  */
 package dev.knative.eventing.kafka.broker.core.oidc;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import dev.knative.eventing.kafka.broker.core.NamespacedName;
 import io.fabric8.kubernetes.api.model.authentication.TokenRequest;
 import io.fabric8.kubernetes.api.model.authentication.TokenRequestBuilder;
@@ -22,23 +24,50 @@ import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import java.util.concurrent.TimeUnit;
 
 public class TokenProvider {
 
+    private static final long TOKEN_EXPIRATION_SECONDS = 3600L; // 1 hour
+    private static final long EXPIRATION_BUFFER_TIME_SECONDS = 300L; // 5 minutes
+
+    private static final long CACHE_MAXIMUM_SIZE = 1000L; //  Cache up to 1000 tokens
+    private static final long CACHE_EXPIRATION_TIME_SECONDS =
+            TOKEN_EXPIRATION_SECONDS - EXPIRATION_BUFFER_TIME_SECONDS; // Cache tokens for 55 minutes
+
     private final KubernetesClient kubernetesClient;
+    private final Cache<String, String> tokenCache;
 
     public TokenProvider() {
         Config clientConfig = new ConfigBuilder().build();
-
         kubernetesClient =
                 new KubernetesClientBuilder().withConfig(clientConfig).build();
+
+        this.tokenCache = CacheBuilder.newBuilder()
+                .expireAfterWrite(CACHE_EXPIRATION_TIME_SECONDS, TimeUnit.SECONDS)
+                .maximumSize(CACHE_MAXIMUM_SIZE)
+                .build();
     }
 
-    public String requestToken(NamespacedName serviceAccount, String audience) {
+    public String getToken(NamespacedName serviceAccount, String audience) {
+        String cacheKey = generateCacheKey(serviceAccount, audience);
+        String token = tokenCache.getIfPresent(cacheKey);
+
+        if (token == null) {
+            token = requestToken(serviceAccount, audience);
+            if (token != null) {
+                tokenCache.put(cacheKey, token);
+            }
+        }
+
+        return token;
+    }
+
+    private String requestToken(NamespacedName serviceAccount, String audience) {
         TokenRequest tokenRequest = new TokenRequestBuilder()
                 .withNewSpec()
                 .withAudiences(audience)
-                .withExpirationSeconds(3600L)
+                .withExpirationSeconds(TOKEN_EXPIRATION_SECONDS)
                 .endSpec()
                 .build();
 
@@ -53,5 +82,9 @@ public class TokenProvider {
         } else {
             return null;
         }
+    }
+
+    private String generateCacheKey(NamespacedName serviceAccount, String audience) {
+        return serviceAccount.namespace() + "/" + serviceAccount.name() + "/" + audience;
     }
 }
