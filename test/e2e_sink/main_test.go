4ba98401a59457549c0c7d6fdd5d2d8506fdcf09
Neeraj Nagure <nagureneeraj@gmail.com>
2024-03-12 21:29:35 +0530

    used new logging package in all e2e test main functions
    Signed-off-by: Neeraj Nagure <nagureneeraj@gmail.com>

--- test/e2e_sink/main_test.go
+++ test/e2e_sink/main_test.go
@@ -22,15 +22,34 @@ package e2e_sink
 import (
 	"os"
 	"testing"
+	"context"
+	"fmt"
+	
+	kubeclient "knative.dev/pkg/client/injection/kube/client"
 
-	testlib "knative.dev/eventing/test/lib"
-	"knative.dev/pkg/system"
+	"knative.dev/eventing-kafka-broker/test/pkg/logging"
+	"knative.dev/reconciler-test/pkg/environment"
 )
+
+// global is the singleton instance of GlobalEnvironment. It is used to parse
+// the testing config for the test run. The config will specify the cluster
+// config as well as the parsing level and state flags.
+var global environment.GlobalEnvironment
 
 func TestMain(m *testing.M) {
 	os.Exit(func() int {
-		defer testlib.ExportLogs(testlib.SystemLogsDir, system.Namespace())
+		ctx, _ := global.Environment()
+		// make sure that this context only cancels after the tests finish running
+		ctx = context.WithoutCancel(ctx)
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
 
+		client := kubeclient.Get(ctx)
+		logger := logging.NewLogger(ctx, client, map[string][]string{"knative-eventing": {"kafka-broker-dispatcher", "kafka-broker-receiver", "kafka-sink-receiver", "kafka-channel-receiver", "kafka-channel-dispatcher", "kafka-source-dispatcher", "kafka-webhook-eventing", "kafka-controller", "kafka-source-controller", "eventing-webhook"}})
+		err := logger.Start()
+		if err != nil {
+			fmt.Printf("failed to start logger: %s", err.Error())
+		}
 		return m.Run()
 	}())
 }
