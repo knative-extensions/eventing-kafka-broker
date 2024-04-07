26e161903a852157079332842c09784d9aafa61f
Neeraj Nagure <nagureneeraj@gmail.com>
2024-04-07 22:20:13 +0530

    moved sacura tests at the end
    Signed-off-by: Neeraj Nagure <nagureneeraj@gmail.com>

--- test/keda-reconciler-tests.sh
+++ test/keda-reconciler-tests.sh
@@ -45,12 +45,12 @@ else
 	echo "BROKER_CLASS is set to '${BROKER_CLASS}'. Running tests for that broker class."
 fi
 
-if ! ${LOCAL_DEVELOPMENT}; then
-	go_test_e2e -tags=sacura -timeout=40m ./test/e2e/... || fail_test "E2E sacura tests failed"
-fi
-
 go_test_e2e -timeout=1h ./test/e2e_new/... || fail_test "E2E (new) suite failed"
 
 go_test_e2e -tags=deletecm ./test/e2e_new/... || fail_test "E2E (new deletecm) suite failed"
+
+if ! ${LOCAL_DEVELOPMENT}; then
+	go_test_e2e -tags=sacura -timeout=40m ./test/e2e/... || fail_test "E2E sacura tests failed"
+fi
 
 success
