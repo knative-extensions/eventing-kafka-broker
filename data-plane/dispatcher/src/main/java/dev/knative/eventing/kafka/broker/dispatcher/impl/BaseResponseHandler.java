2263a6bd159cf79869520ca77400b1fb5a067bd4
Calum Murray <cmurray@redhat.com>
2024-08-20 11:01:35 -0400

    fix: base response handler build issue is resolved
    Signed-off-by: Calum Murray <cmurray@redhat.com>

--- data-plane/dispatcher/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/impl/BaseResponseHandler.java
+++ data-plane/dispatcher/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/impl/BaseResponseHandler.java
@@ -52,6 +52,7 @@ public abstract class BaseResponseHandler implements ResponseHandler {
         this.eventTypeLister = eventTypeLister;
         this.reference = reference;
         this.isEventTypeAutocreateEnabled = true;
+        return this;
     }
 
     /**
