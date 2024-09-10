b7f78afc3c99e464b38e89994a0ec9869c46f349
Martin Gencur <mgencur@redhat.com>
2024-09-10 14:18:37 +0200

    Paired upgrade tests

--- test/upgrade/smoke.go
+++ test/upgrade/smoke.go
@@ -18,77 +18,149 @@ package upgrade
 
 import (
 	"context"
-	"testing"
+	"sync"
 
-	cloudevents "github.com/cloudevents/sdk-go/v2"
-	pkgtesting "knative.dev/eventing-kafka-broker/test/pkg"
-	testbroker "knative.dev/eventing-kafka-broker/test/pkg/broker"
+	"github.com/google/uuid"
+	"knative.dev/eventing-kafka-broker/control-plane/pkg/kafka"
 	"knative.dev/eventing-kafka-broker/test/rekt/features"
-	eventinghelpers "knative.dev/eventing/test/e2e/helpers"
-	testlib "knative.dev/eventing/test/lib"
+	brokerfeatures "knative.dev/eventing/test/rekt/features/broker"
+	channelfeatures "knative.dev/eventing/test/rekt/features/channel"
+	"knative.dev/eventing/test/rekt/features/knconf"
+	brokerresources "knative.dev/eventing/test/rekt/resources/broker"
+	"knative.dev/eventing/test/rekt/resources/channel_impl"
+	"knative.dev/eventing/test/rekt/resources/subscription"
+	eventingupgrade "knative.dev/eventing/test/upgrade"
+	duckv1 "knative.dev/pkg/apis/duck/v1"
 	"knative.dev/pkg/system"
 	"knative.dev/reconciler-test/pkg/environment"
+	"knative.dev/reconciler-test/pkg/eventshub"
+	"knative.dev/reconciler-test/pkg/feature"
 	"knative.dev/reconciler-test/pkg/k8s"
 	"knative.dev/reconciler-test/pkg/knative"
+	"knative.dev/reconciler-test/pkg/manifest"
 )
 
 var (
-	channelTestRunner testlib.ComponentsTestRunner
-)
-
-func runBrokerSmokeTest(t *testing.T, class string) {
-	pkgtesting.RunMultiple(t, func(t *testing.T) {
-		eventinghelpers.EventTransformationForTriggerTestHelper(
-			context.Background(),
-			t,
-			/* broker version */ "v1",
-			/* trigger version */ "v1",
-			testbroker.CreatorForClass(class),
-		)
-	})
-}
-
-func runChannelSmokeTest(t *testing.T) {
-	cases := smokeTestCases()
-	ctx := context.Background()
-	for i := range cases {
-		tt := cases[i]
-		t.Run(tt.name, func(t *testing.T) {
-			eventinghelpers.SingleEventForChannelTestHelper(
-				ctx, t, tt.encoding, tt.version,
-				"", channelTestRunner,
-			)
-		})
-	}
-}
-
-func runSourceSmokeTest(glob environment.GlobalEnvironment, t *testing.T) {
-	ctx, env := glob.Environment(
+	brokerConfigMux  = &sync.Mutex{}
+	channelConfigMux = &sync.Mutex{}
+	opts             = []environment.EnvOpts{
 		knative.WithKnativeNamespace(system.Namespace()),
 		knative.WithLoggingConfig,
 		knative.WithTracingConfig,
 		k8s.WithEventListener,
-		environment.Managed(t),
-	)
+	}
+)
 
-	env.Test(ctx, t, features.KafkaSourceStructuredEvent())
-	env.Test(ctx, t, features.KafkaSourceBinaryEvent())
+func KafkaChannelFeature(glob environment.GlobalEnvironment) *eventingupgrade.DurableFeature {
+	// Prevent race conditions on channel_impl.EnvCfg.ChannelGK when running tests in parallel.
+	channelConfigMux.Lock()
+	defer channelConfigMux.Unlock()
+	channel_impl.EnvCfg.ChannelGK = "KafkaChannel.messaging.knative.dev"
+	channel_impl.EnvCfg.ChannelV = "v1beta1"
+
+	createSubscriberFn := func(ref *duckv1.KReference, uri string) manifest.CfgFn {
+		return subscription.WithSubscriber(ref, uri, "")
+	}
+
+	setupF := feature.NewFeature()
+	sink, ch := channelfeatures.ChannelChainSetup(setupF, 1, createSubscriberFn)
+
+	verifyF := func() *feature.Feature {
+		f := feature.NewFeatureNamed(setupF.Name)
+		channelfeatures.ChannelChainAssert(f, sink, ch)
+		return f
+	}
+
+	return &eventingupgrade.DurableFeature{SetupF: setupF, VerifyF: verifyF, Global: glob, EnvOpts: opts}
 }
 
-type smokeTestCase struct {
-	name     string
-	encoding cloudevents.Encoding
-	version  eventinghelpers.SubscriptionVersion
+func KafkaSinkSourceBinaryEventFeature(glob environment.GlobalEnvironment,
+) *eventingupgrade.DurableFeature {
+	setupF := feature.NewFeature()
+	kafkaSink, receiver := features.KafkaSourceBinaryEventFeatureSetup(setupF)
+
+	verifyF := func() *feature.Feature {
+		f := feature.NewFeatureNamed(setupF.Name)
+		features.KafkaSourceFeatureAssert(f, kafkaSink, receiver, features.KafkaSourceBinaryEventCustomizeFunc())
+		return f
+	}
+
+	return &eventingupgrade.DurableFeature{SetupF: setupF, VerifyF: verifyF, Global: glob, EnvOpts: opts}
 }
 
-func smokeTestCases() []smokeTestCase {
-	return []smokeTestCase{{
-		name:     "BinaryV1",
-		encoding: cloudevents.EncodingBinary,
-		version:  eventinghelpers.SubscriptionV1,
-	}, {
-		name:     "StructuredV1",
-		encoding: cloudevents.EncodingStructured,
-		version:  eventinghelpers.SubscriptionV1,
-	}}
+func KafkaSinkSourceStructuredEventFeature(glob environment.GlobalEnvironment,
+) *eventingupgrade.DurableFeature {
+	setupF := feature.NewFeature()
+	kafkaSink, receiver := features.KafkaSourceStructuredEventFeatureSetup(setupF)
+	verifyF := func() *feature.Feature {
+		f := feature.NewFeatureNamed(setupF.Name)
+		features.KafkaSourceFeatureAssert(f, kafkaSink, receiver, features.KafkaSourceStructuredEventCustomizeFunc())
+		return f
+	}
+
+	return &eventingupgrade.DurableFeature{SetupF: setupF, VerifyF: verifyF, Global: glob, EnvOpts: opts}
+}
+
+func BrokerEventTransformationForTrigger(glob environment.GlobalEnvironment,
+) *eventingupgrade.DurableFeature {
+	// Prevent race conditions on EnvCfg.BrokerClass when running tests in parallel.
+	brokerConfigMux.Lock()
+	defer brokerConfigMux.Unlock()
+	brokerresources.EnvCfg.BrokerClass = kafka.BrokerClass
+
+	setupF := feature.NewFeature()
+	cfg := brokerfeatures.BrokerEventTransformationForTriggerSetup(setupF)
+
+	verifyF := func() *feature.Feature {
+		f := feature.NewFeatureNamed(setupF.Name)
+		brokerfeatures.BrokerEventTransformationForTriggerAssert(f, cfg)
+		return f
+	}
+
+	return &eventingupgrade.DurableFeature{SetupF: setupF, VerifyF: verifyF, Global: glob, EnvOpts: opts}
+}
+
+func NamespacedBrokerEventTransformationForTrigger(glob environment.GlobalEnvironment,
+) *eventingupgrade.DurableFeature {
+	// Prevent race conditions on EnvCfg.BrokerClass when running tests in parallel.
+	brokerConfigMux.Lock()
+	defer brokerConfigMux.Unlock()
+	brokerresources.EnvCfg.BrokerClass = kafka.NamespacedBrokerClass
+
+	broker := "broker"
+	setupF := features.SetupNamespacedBroker(broker)
+	// Override name to match the enclosing function name.
+	setupF.Name = feature.NewFeature().Name
+
+	verifyF := func() *feature.Feature {
+		f := feature.NewFeatureNamed(setupF.Name)
+		brokerAcceptsBinaryContentModeAssert(f, broker)
+		return f
+	}
+
+	return &eventingupgrade.DurableFeature{SetupF: setupF, VerifyF: verifyF, Global: glob, EnvOpts: opts}
+}
+
+func brokerAcceptsBinaryContentModeAssert(f *feature.Feature, brokerName string) {
+	f.Assert("broker accepts binary content mode", func(ctx context.Context, t feature.T) {
+		source := feature.MakeRandomK8sName("source")
+		eventshub.Install(source,
+			eventshub.StartSenderToResource(brokerresources.GVR(), brokerName),
+			eventshub.InputHeader("ce-specversion", "1.0"),
+			eventshub.InputHeader("ce-type", "sometype"),
+			eventshub.InputHeader("ce-source", "200.request.sender.test.knative.dev"),
+			eventshub.InputHeader("ce-id", uuid.New().String()),
+			eventshub.InputHeader("content-type", "application/json"),
+			eventshub.InputBody("{}"),
+			eventshub.InputMethod("POST"),
+		)(ctx, t)
+
+		store := eventshub.StoreFromContext(ctx, source)
+		events := knconf.Correlate(store.AssertAtLeast(ctx, t, 2, knconf.SentEventMatcher("")))
+		for _, e := range events {
+			if e.Response.StatusCode < 200 || e.Response.StatusCode > 299 {
+				t.Errorf("Expected statuscode 2XX for sequence %d got %d", e.Response.Sequence, e.Response.StatusCode)
+			}
+		}
+	})
 }
