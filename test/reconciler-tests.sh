7aa36563fafb32b83fae124e171d0f3aa4a064bf
Pierangelo Di Pilato <pierdipi@redhat.com>
2024-11-19 13:02:06 +0100

    Increase nodes
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- test/reconciler-tests.sh
+++ test/reconciler-tests.sh
@@ -4,7 +4,7 @@ source $(dirname "$0")/e2e-common.sh
 export BROKER_TEMPLATES=./templates/kafka-broker
 
 if ! ${SKIP_INITIALIZE}; then
-	initialize "$@" --num-nodes=4
+	initialize "$@" --num-nodes=5
 	save_release_artifacts || fail_test "Failed to save release artifacts"
 fi
 
