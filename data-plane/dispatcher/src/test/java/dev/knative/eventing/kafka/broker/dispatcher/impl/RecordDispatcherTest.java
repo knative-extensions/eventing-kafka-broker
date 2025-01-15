5944f0f5d4c356e36d43de76b52d49db13eae220
Matthias Wessendorf <mwessend@redhat.com>
2025-01-15 11:41:01 +0100

    Align w/ Quarkus 3.15 and friends Update breaking (micrometer/vertx) API calls
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- data-plane/dispatcher/src/test/java/dev/knative/eventing/kafka/broker/dispatcher/impl/RecordDispatcherTest.java
+++ data-plane/dispatcher/src/test/java/dev/knative/eventing/kafka/broker/dispatcher/impl/RecordDispatcherTest.java
@@ -42,8 +42,8 @@ import dev.knative.eventing.kafka.broker.dispatcher.main.ConsumerVerticleContext
 import dev.knative.eventing.kafka.broker.dispatcher.main.FakeConsumerVerticleContext;
 import io.cloudevents.CloudEvent;
 import io.micrometer.core.instrument.search.MeterNotFoundException;
-import io.micrometer.prometheus.PrometheusConfig;
-import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.micrometer.prometheusmetrics.PrometheusConfig;
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 import io.vertx.core.Future;
 import io.vertx.core.MultiMap;
 import io.vertx.core.buffer.Buffer;
@@ -73,9 +73,11 @@ public class RecordDispatcherTest {
                     .build());
 
     static {
-        BackendRegistries.setupBackend(new MicrometerMetricsOptions()
-                .setMicrometerRegistry(new PrometheusMeterRegistry(PrometheusConfig.DEFAULT))
-                .setRegistryName(Metrics.METRICS_REGISTRY_NAME));
+        BackendRegistries.setupBackend(
+                new MicrometerMetricsOptions()
+                        .setMicrometerRegistry(new PrometheusMeterRegistry(PrometheusConfig.DEFAULT))
+                        .setRegistryName(Metrics.METRICS_REGISTRY_NAME),
+                null);
     }
 
     private PrometheusMeterRegistry registry;
