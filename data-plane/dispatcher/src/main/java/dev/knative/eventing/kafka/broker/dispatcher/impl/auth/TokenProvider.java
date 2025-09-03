b4571d5f28b625e6f65902bfad14923128f08840
Martin Simka <msimka@redhat.com>
2025-09-03 13:52:58 +0200

    [SRVKE-1749] reuse existing Vert.x instance in Kubernetes Client
    https://issues.redhat.com/browse/SRVKE-1749
    
    fixes thread and file descriptor leak

--- data-plane/dispatcher/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/impl/auth/TokenProvider.java
+++ data-plane/dispatcher/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/impl/auth/TokenProvider.java
@@ -23,6 +23,7 @@ import io.fabric8.kubernetes.api.model.authentication.TokenRequestBuilder;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import io.fabric8.kubernetes.client.vertx.VertxHttpClientFactory;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import java.util.concurrent.TimeUnit;
@@ -45,6 +46,7 @@ public class TokenProvider {
         this.vertx = vertx;
         this.kubernetesClient = new KubernetesClientBuilder()
                 .withConfig(new ConfigBuilder().build())
+                .withHttpClientFactory(new VertxHttpClientFactory(vertx))
                 .build();
         this.tokenCache = CacheBuilder.newBuilder()
                 .expireAfterWrite(CACHE_EXPIRATION_TIME_SECONDS, TimeUnit.SECONDS)
