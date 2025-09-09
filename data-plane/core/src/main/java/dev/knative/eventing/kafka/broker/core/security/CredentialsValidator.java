4c5a285bdd9573fe54a938db34ab8245bb998b5c
Quenby Mitchell <quenby.mitchell@thoughtfulautomation.com>
2025-09-09 11:17:21 -0600

    fix formatting

--- data-plane/core/src/main/java/dev/knative/eventing/kafka/broker/core/security/CredentialsValidator.java
+++ data-plane/core/src/main/java/dev/knative/eventing/kafka/broker/core/security/CredentialsValidator.java
@@ -63,8 +63,7 @@ class CredentialsValidator {
                         + SASLMechanism;
             }
             if (anyBlank(credentials.SASLUsername(), credentials.SASLPassword())
-                    && (SASLMechanism == null
-                    || !"OAUTHBEARER".equals(SASLMechanism))) {
+                    && (SASLMechanism == null || !"OAUTHBEARER".equals(SASLMechanism))) {
                 return "Security protocol " + securityProtocol.name + ":  invalid SASL username or password";
             }
             return null;
